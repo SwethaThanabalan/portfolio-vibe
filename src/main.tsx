b693d38081b895657f0029086d0a5ed97f0684e7
SwethaThanabalan <62706498+SwethaThanabalan@users.noreply.github.com>
2026-02-24 20:46:29 -0500

    Update main.tsx for https redirect

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Redirect HTTP to HTTPS
+if (window.location.protocol === 'http:' && window.location.hostname !== 'localhost') {
+  window.location.href = window.location.href.replace('http:', 'https:')
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
